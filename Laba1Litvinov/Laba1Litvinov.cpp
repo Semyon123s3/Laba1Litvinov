0b21e4f7b16b3a017db585d844b5f8e9c904ae42
Semyon123s3 <133234409+Semyon123s3@users.noreply.github.com>
2025-09-18 00:35:14 +0300

    Добавление функций ввода для трубы и КС

--- Laba1Litvinov/Laba1Litvinov.cpp
+++ Laba1Litvinov/Laba1Litvinov.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include <windows.h>
+#include <string>
 using namespace std;
 
 struct Pipe {
@@ -8,6 +9,7 @@ struct Pipe {
     float length;
     int diametr;
     bool status;
+    bool isAdded = false;
 };
 
 struct CS {
@@ -15,6 +17,7 @@ struct CS {
     int number_work;
     int number_work_online;
     string class_cs;
+    bool isAdded = false;
 };
 
 void Show_menu() {
@@ -28,6 +31,40 @@ void Show_menu() {
     cout << "0. Выход" << endl;
     cout << "Выберите действие: ";
 }
+
+void add_Truba(Pipe t) {
+    cout << "Добавление трубы" << endl;
+    cout << "Введите килметровую отметку(название): ";
+    cin.ignore();
+    getline(cin, t.name);
+    cout << "Введите длину трубы (км): ";
+    cin >> t.length;
+    cout << "Введите диаметр трубы (мм): ";
+    cin >> t.diametr;
+    cout << "В ремонте (1-да, 0-нет): ";
+    cin >> t.status;
+    t.isAdded = true;
+    cout << "Труба добавлена/изменена!" << endl;
+}
+void add_CS(CS k) {
+    cout << "Название: ";
+    cin.ignore();
+    getline(cin, k.name);
+    cout << "Всего цехов: ";
+    cin >> k.number_work;
+    cout << "Работающих цехов: ";
+    cin >> k.number_work_online;
+    cout << "Класс: ";
+    cin >> k.class_cs;
+    k.isAdded = true;
+    cout << "КС добавлена/изменена!" << endl;
+}
+
+
+
+
+
+
 
 int main()
 {
